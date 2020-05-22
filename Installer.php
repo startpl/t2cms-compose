90eed3f68d87cb993a19447b7d5eb16fb7dc97fa
koperdog <checnev1997@yandex.ru>
2020-05-22 17:06:50 +0500

    Added support for extra bootstrap

--- Installer.php
+++ Installer.php
@@ -7,9 +7,13 @@ use Composer\Package\PackageInterface;
 use Composer\Repository\InstalledRepositoryInterface;
 use Composer\Installer\LibraryInstaller;
 use t2cms\composer\installers\T2cmsInstaller;
+use Composer\Util\Filesystem;
 
 class Installer extends LibraryInstaller
 {
+    const EXTRA_BOOTSTRAP = 'bootstrap';
+    const EXTENSION_FILE = 'yiisoft/extensions.php';
+    
     const PREFIX_TYPE = 't2cms';
     
     /**
@@ -20,7 +24,25 @@ class Installer extends LibraryInstaller
         $installer = new T2cmsInstaller($package, $this->composer, $this->getIO());
         return $installer->getInstallPath($package, self::PREFIX_TYPE);
     }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        // install the package the normal composer way
+        parent::install($repo, $package);
+        // add the package to yiisoft/extensions.php
+        $this->addPackage($package);
+        // ensure the yii2-dev package also provides Yii.php in the same place as yii2 does
+        if ($package->getName() == 'yiisoft/yii2-dev') {
+            $this->linkBaseYiiFiles();
+        }
+    }
 
+    /**
+     * {@inheritDoc}
+     */
     public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
         parent::uninstall($repo, $package);
@@ -58,5 +80,115 @@ class Installer extends LibraryInstaller
     private function getIO()
     {
         return $this->io;
+    }
+    
+    protected function addPackage(PackageInterface $package)
+    {
+        $extension = [
+            'name' => $package->getName(),
+            'version' => $package->getVersion(),
+        ];
+
+        $alias = $this->generateDefaultAlias($package);
+        if (!empty($alias)) {
+            $extension['alias'] = $alias;
+        }
+        $extra = $package->getExtra();
+        if (isset($extra[self::EXTRA_BOOTSTRAP])) {
+            $extension['bootstrap'] = $extra[self::EXTRA_BOOTSTRAP];
+        }
+
+        $extensions = $this->loadExtensions();
+        $extensions[$package->getName()] = $extension;
+        $this->saveExtensions($extensions);
+    }
+    
+    protected function loadExtensions()
+    {
+        $file = $this->vendorDir . '/' . static::EXTENSION_FILE;
+        if (!is_file($file)) {
+            return [];
+        }
+        // invalidate opcache of extensions.php if exists
+        if (function_exists('opcache_invalidate')) {
+            @opcache_invalidate($file, true);
+        }
+        $extensions = require($file);
+
+        $vendorDir = str_replace('\\', '/', $this->vendorDir);
+        $n = strlen($vendorDir);
+
+        foreach ($extensions as &$extension) {
+            if (isset($extension['alias'])) {
+                foreach ($extension['alias'] as $alias => $path) {
+                    $path = str_replace('\\', '/', $path);
+                    if (strpos($path . '/', $vendorDir . '/') === 0) {
+                        $extension['alias'][$alias] = '<vendor-dir>' . substr($path, $n);
+                    }
+                }
+            }
+        }
+
+        return $extensions;
+    }
+
+    protected function saveExtensions(array $extensions)
+    {
+        $file = $this->vendorDir . '/' . static::EXTENSION_FILE;
+        if (!file_exists(dirname($file))) {
+            mkdir(dirname($file), 0777, true);
+        }
+        $array = str_replace("'<vendor-dir>", '$vendorDir . \'', var_export($extensions, true));
+        file_put_contents($file, "<?php\n\n\$vendorDir = dirname(__DIR__);\n\nreturn $array;\n");
+        // invalidate opcache of extensions.php if exists
+        if (function_exists('opcache_invalidate')) {
+            @opcache_invalidate($file, true);
+        }
+    }
+    
+    protected function generateDefaultAlias(PackageInterface $package)
+    {
+        $fs = new Filesystem;
+        $vendorDir = $fs->normalizePath($this->vendorDir);
+        $autoload = $package->getAutoload();
+
+        $aliases = [];
+
+        if (!empty($autoload['psr-0'])) {
+            foreach ($autoload['psr-0'] as $name => $path) {
+                $name = str_replace('\\', '/', trim($name, '\\'));
+                if (!$fs->isAbsolutePath($path)) {
+                    $path = $this->vendorDir . '/' . $package->getPrettyName() . '/' . $path;
+                }
+                $path = $fs->normalizePath($path);
+                if (strpos($path . '/', $vendorDir . '/') === 0) {
+                    $aliases["@$name"] = '<vendor-dir>' . substr($path, strlen($vendorDir)) . '/' . $name;
+                } else {
+                    $aliases["@$name"] = $path . '/' . $name;
+                }
+            }
+        }
+
+        if (!empty($autoload['psr-4'])) {
+            foreach ($autoload['psr-4'] as $name => $path) {
+                if (is_array($path)) {
+                    // ignore psr-4 autoload specifications with multiple search paths
+                    // we can not convert them into aliases as they are ambiguous
+                    continue;
+                }
+                $name = str_replace('\\', '/', trim($name, '\\'));
+                if (!$fs->isAbsolutePath($path)) {
+                    $path = $this->vendorDir . '/' . $package->getPrettyName() . '/' . $path;
+                }
+                $path = $fs->normalizePath($path);
+                if (strpos($path . '/', $vendorDir . '/') === 0) {
+                    $aliases["@$name"] = '<vendor-dir>' . substr($path, strlen($vendorDir));
+                } else {
+                    $aliases["@$name"] = $path;
+                }
+            }
+        }
+
+        return $aliases;
     }
 }
